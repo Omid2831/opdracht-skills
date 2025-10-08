6a15a4cf04f98020be25b1b5daf2ffe153309753
Omid <mehrabiomid8282@gmail.com>
2025-10-08 13:17:37 +0200

    feat: first homepage sample 'will be developed '

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,20 @@
-<h2> here you go you are in the webpage</h2>
+<h2> {{ $t }}</h2>
+
+
+
+<table class="table-auto">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+    </tr>
+  </thead>
+  <tbody>        
+    @foreach ( $animal as  $a )
+    <tr>
+      <td>{{ $a->Id }}</td>
+      <td>{{ $a->Name }}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
