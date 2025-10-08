2e7f0b9bb9b9e2b8d3cb24eaf658fa837e4fc428
Omid <mehrabiomid8282@gmail.com>
2025-10-08 17:12:37 +0200

    fix(router): intertia has been deleted

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,8 @@
 
 use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-// Route::get('/', function () {
-//     return Inertia::render('welcome');
-// })->name('home');
 
 Route::get('/',[AnimalController::class,  'index'])->name('homepage');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
