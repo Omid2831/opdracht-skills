2ef32570f4bd04ec18d1fa4deab6b9b765a54999
Omid <mehrabiomid8282@gmail.com>
2025-10-08 13:16:17 +0200

    feat: getting the data from the database

--- app/Models/AnimalModel.php
+++ app/Models/AnimalModel.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Database\Eloquent\Model;
 
 class AnimalModel extends Model
 {
-    //
+    public static function getAllAnimals(){
+        
+       $query = DB::select('CALL sp_getAllBabyanimal()');
+
+       return $query;
+    }
 }
