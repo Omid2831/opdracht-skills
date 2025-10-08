31a85d3a915df671861cf8e650f719ddace9eb2c
Omid <mehrabiomid8282@gmail.com>
2025-10-08 17:25:58 +0200

    feat: igriding the images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,17 +3,16 @@
 
 
 @section('content')
-    <div class="bg-gray-600 border-4 w-f h-45">
+    <div class=" grid grid-cols-1 md:grid-cols-3">
 
         <!-- Table here -->
         @foreach ($animal as $a)
             <div class="flex flex-col">
                 <a>
-                    <img src="./assets/animalcards/{{ $a->NOTE }}" alt="logo {{$a->Id}}">
+                    <img src="./assets/animalcards/{{ $a->NOTE }}" alt="logo {{$a->Id}}" class="w-40 h-40 m-2 rounded-lg border-dotted border-4">
                 </a>
             </div>
         @endforeach
     </div>
     
 @endsection
-`
