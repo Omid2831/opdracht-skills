44c0fcfa4e7a06c275b3143ecaa4c856f3d1b831
Omid <mehrabiomid8282@gmail.com>
2025-10-08 12:46:53 +0200

    feat(router): giving the right path

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
+
+Route::get('/',[AnimalController::class,  'index'])->name('homepage');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
