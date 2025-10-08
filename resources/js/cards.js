e11e05ff6d3ffd397ebf3f61a739b298d2572810
Omid <mehrabiomid8282@gmail.com>
2025-10-08 20:11:07 +0200

    feat: add adult name conversion function to cards.js

--- resources/js/cards.js
+++ resources/js/cards.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
     // define the data 
     const deckInner = document.getElementById('deckInner');
     const deckFront = document.getElementById('deckFront');
+
+    //finding the name of the adult image from the baby image name 
+    function adultName(baby){
+        return baby.replace('_baby','_adult');
+    }
 })
