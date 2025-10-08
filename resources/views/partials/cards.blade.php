96e43ff2cb5697e9a5c2fae05be0c54e387cc7b6
Omid <mehrabiomid8282@gmail.com>
2025-10-08 19:33:45 +0200

    feat: add deck image display to cards partial

---
+++ resources/views/partials/cards.blade.php
@@ -0,0 +1,5 @@
+<!-- Deck image on the left -->
+        <div class="ml-8 hidden lg:flex items-start justify-start">
+            <img src="{{ asset('assets/backside.png') }}" alt="Deck"
+                class="w-48 h-auto transform -translate-y-4 shadow-lg">
+        </div>
