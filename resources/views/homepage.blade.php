484c608165d919b7f56760a521a2d66e9406c4de
Omid <mehrabiomid8282@gmail.com>
2025-10-08 13:38:20 +0200

    feat : add the starting data

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,20 +1,14 @@
-<h2> {{ $t }}</h2>
+@extends('layouts.app')
 
 
 
-<table class="table-auto">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-    </tr>
-  </thead>
-  <tbody>        
-    @foreach ( $animal as  $a )
-    <tr>
-      <td>{{ $a->Id }}</td>
-      <td>{{ $a->Name }}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
+@section('content')
+
+<div class="bg-gray-600 border-4 w-f h-45">
+
+  <!-- Table here 
+
+</div>
+
+
+@endsection
