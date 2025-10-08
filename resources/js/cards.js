e0ce3efa3e43bce84f51c3331fb91b7eb3dfee8f
Omid <mehrabiomid8282@gmail.com>
2025-10-08 20:08:46 +0200

    feat: initialize card deck structure on DOMContentLoaded

---
+++ resources/js/cards.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // define the data 
+    const deckInner = document.getElementById('deckInner');
+    const deckFront = document.getElementById('deckFront');
+})
