bea8ff9a34f2c9860bf93ca7f90c4a8fb3180b7e
Omid <mehrabiomid8282@gmail.com>
2025-10-08 11:47:32 +0200

    feat: giving the folder structure the accessibilty to the other folders under the js folder

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,21 +8,29 @@ import { initializeTheme } from './hooks/use-appearance';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.tsx'),
-        ),
+    title: (title) => `${title} - ${appName}`,
+    resolve: async (name) => {
+        // Convert dot notation to folder structure
+        const path = `./Pages/${name.replace(/\./g, '/')}.jsx`;
+
+        // import.meta.glob returns an object of functions
+        const pages = import.meta.glob('./Pages/**/*.jsx');
+
+        if (!pages[path]) {
+            throw new Error(`Page not found: ${path}`);
+        }
+
+        return pages[path]();
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
-
         root.render(<App {...props} />);
     },
     progress: {
         color: '#4B5563',
     },
 });
+
 
 // This will set light / dark mode on load...
 initializeTheme();
