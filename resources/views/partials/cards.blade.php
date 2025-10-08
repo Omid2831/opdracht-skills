76f95f9c311371828e7960fae8b1b05213316b02
Omid <mehrabiomid8282@gmail.com>
2025-10-08 18:48:55 +0200

    feat: deck of cards

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -11,6 +11,32 @@
                 </div>
             </div>
 
+            <div id="deck" class="grid grid-cols-3 gap-6">
+                    @php
+
+                        if (!isset($cards)) {
+                            $cards = [
+                                'cat_adult.png',
+                                'cow_adult.png',
+                                'dog_adult.png',
+                                'duck_adult.png',
+                                'elephant_adult.png',
+                                'goat_adult.png',
+                                'horse_adult.png',
+                                'pig_adult.png',
+                                'seal_adult.png',
+                            ];
+                        }
+                    @endphp
+
+                    @foreach ($cards as $card)
+                        <div class="flex justify-center items-center">
+                            <img src="{{ asset('assets/animalcards/' . $card) }}" alt="{{ $card }}"
+                                class="w-40 h-40 border-4 border-dotted border-white rounded">
+                        </div>
+                    @endforeach
+                </div>
+
             </div>
         </div>
 @endsection
