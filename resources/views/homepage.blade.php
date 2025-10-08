547842ff818c9edd5a41942fa242b3cf3f488866
Omid <mehrabiomid8282@gmail.com>
2025-10-08 14:00:47 +0200

    feat: the images here are setted

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,12 +3,15 @@
 
 
 @section('content')
+    <div class="bg-gray-600 border-4 w-f h-45">
 
-<div class="bg-gray-600 border-4 w-f h-45">
-
-  <!-- Table here 
-
-</div>
-
-
+        <!-- Table here -->
+        @foreach ($animal as $a)
+            <div class="flex flex-col">
+                <a>
+                    <img src="{{ './assets/animalcards/$a->NOTE' }}" alt="logo {{$a->Id}}">
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
