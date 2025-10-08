80c834ed68e1fb3f1776d42984f186d313d61c1f
Omid <mehrabiomid8282@gmail.com>
2025-10-08 18:37:24 +0200

    feat: bringing the card  `backside` to homepage

---
+++ resources/views/partials/cards.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-7xl mx-auto py-8">
+        <div class="grid grid-cols-12 gap-6 items-start">
+            
+            <div class="col-span-5 flex items-center justify-center">
+                <div class="w-72 h-96 relative">
+                    <img src="{{ asset('assets/backside.png') }}" alt="backside"
+                        class=" object-cover rounded-lg shadow-2xl">
+                </div>
+            </div>
+
+            </div>
+        </div>
+@endsection
