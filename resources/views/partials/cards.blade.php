5d0c5a5a6d8280ef1aa0286a83764dec825a120f
Omid <mehrabiomid8282@gmail.com>
2025-10-08 20:06:58 +0200

    feat: add functional deck structure to cards partial

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -5,4 +5,13 @@
     <img src="{{ asset('assets/backside.png') }}" alt="Deck"
         class="w-48 h-auto shadow-lg absolute top-2 left-4 transform rotate-10">
 
+    <!-- functional deck -->
+    <div id="deck" class="relative mt-10 w-[19rem] h-[19rem] perspective-[1000px]">
+        <div id="deckInner" class="w-full h-full relative">
+            <img id="deckBack" src="{{ asset('assets/backside.png') }}" alt="Deck back"
+                class="absolute w-full h-full object-contain rotate-y-0 backface-hidden">
+            <img id="deckFront" src="" alt="Deck front"
+                class="absolute w-full h-full object-contain rotate-y-180 backface-hidden">
+        </div>
+    </div>
 </div>
